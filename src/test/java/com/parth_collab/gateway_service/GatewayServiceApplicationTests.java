cdd67da725bd848d73f28fc2aba93c3f7e54a2c2
parth9906 <parth.sachan.parth@gmail.com>
2025-10-06 19:18:51 +0530

    first commit

---
+++ src/test/java/com/parth_collab/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parth_collab.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
